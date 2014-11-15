3f7cbcd0cb2fa0a2bec14a5ee288988e05816149
drkitty <bumblingkitty@gmail.com>
2014-11-14 23:17:30 -0800

    Generate binary image, not hex

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-avrdude -p m2560 -c stk500v2 -P /dev/ttyACM0 -U flash:w:blink.hex
+avrdude -p m2560 -c stk500v2 -P /dev/ttyACM0 -U flash:w:blink.bin
--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 avr-gcc -std=c99 -Wall -mmcu=avr6 -mmcu=atmega2560 -Os -DF_CPU=16000000L -o blink blink.c
-avr-objcopy -O ihex blink blink.hex
+avr-objcopy -O binary blink blink.bin
