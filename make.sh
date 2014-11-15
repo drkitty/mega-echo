128940e88b879658aac652e73c2f76b7aca1ef5d
drkitty <bumblingkitty@gmail.com>
2014-11-14 23:16:46 -0800

    Change name to blink

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-avr-gcc -std=c99 -Wall -mmcu=avr6 -mmcu=atmega2560 -Os -DF_CPU=16000000L -o test test.c
-avr-objcopy -O ihex test test.hex
+avr-gcc -std=c99 -Wall -mmcu=avr6 -mmcu=atmega2560 -Os -DF_CPU=16000000L -o blink blink.c
+avr-objcopy -O ihex blink blink.hex
